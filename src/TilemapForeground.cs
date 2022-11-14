a81ccc71820db71087d84a06e03c00e2447f738b
vaporvee <yannik@vaporvee.com>
2022-11-14 22:25:42 +0100

    godot needs a special array :(

--- src/TilemapForeground.cs
+++ src/TilemapForeground.cs
@@ -7,13 +7,23 @@ public class TilemapForeground : TileMap
     bool ReplaceStaticTilesWithNodes = true;
     public override void _Process(float delta)
     {
-     if(ReplaceStaticTilesWithNodes){
-        ReplaceStaticTiles();
-     }
+
     }
 
-    public void ReplaceStaticTiles()
+    public override void _Ready()
     {
+        if (ReplaceStaticTilesWithNodes)
+        {
+            ReplaceStaticTiles(1);
+        }
+    }
 
+    public void ReplaceStaticTiles(int CellID)
+    {
+       //Vector2[] AllCells = GetUsedCellsById(CellID); //why isnt it just a normal array wtf
+       // for(int i = 0; i == AllCells.Length; i++)
+       // {
+       //     GD.Print(AllCells[i]);
+       // }
     }
 }
