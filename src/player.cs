2e97d1fec77a5a46cea8908fcfab028842724d3b
vaporvee <yannik@vaporvee.com>
2022-11-25 00:31:57 +0100

    completely rewritten with godot 4 beta

--- src/player.cs
+++ src/player.cs
@@ -1,45 +1,26 @@
 using Godot;
 using System;
-
-public class player : KinematicBody2D
+public partial class player : CharacterBody2D
 {
-    [Export]
-    public int speed = 400;
+	[Export]
+	public int speed = 400;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
 
-    public override void _PhysicsProcess(float delta)
-    {
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _PhysicsProcess(double delta)
+	{
         MoveAndCollide(new Vector2
             (
-            Input.GetActionStrength("move_right") 
-            - Input.GetActionStrength("move_left"), 
-            Input.GetActionStrength("move_down") 
+            Input.GetActionStrength("move_right")
+            - Input.GetActionStrength("move_left"),
+            Input.GetActionStrength("move_down")
             - Input.GetActionStrength("move_up")
-            ).LimitLength(1) 
-            * speed * delta
+            ).LimitLength(1)
+            * speed * (float)delta
             );
-    }
-
-    public override void _Process(float delta)
-    {
-        //debug the grid
-        int currentCellID = 1;
-        var tilemap = GetNode<TileMap>("/root/Main/World/Foreground");
-        Vector2 coordinates = tilemap.WorldToMap(Position);
-        if (Input.IsActionJustReleased("debug"))
-        { 
-        GD.Print("All Number 1 tiles: " + tilemap.GetUsedCellsById(1));
-        GD.Print("Player coordinate: " + coordinates);
-        }
-        if (Input.IsKeyPressed((int)KeyList.F1))
-        {
-            tilemap.SetCell((int)coordinates.x, (int)coordinates.y, -1);
-        }
-        if (Input.IsKeyPressed((int)KeyList.F2))
-        {
-            if(tilemap.GetCell((int)coordinates.x, (int)coordinates.y) == -1)
-            {
-            tilemap.SetCell((int)coordinates.x, (int)coordinates.y, currentCellID); //place offset coordinates += offset = build coordinates
-            }
-        }
     }
 }
