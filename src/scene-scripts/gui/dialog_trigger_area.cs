b719aae87dc36ebd9d1c5b9b1c8fddf16054bd3d
vaporvee <yannik@vaporvee.com>
2022-11-29 20:06:42 +0100

    json dialog implementation progression

--- src/scene-scripts/gui/dialog_trigger_area.cs
+++ src/scene-scripts/gui/dialog_trigger_area.cs
@@ -1,13 +1,14 @@
 using Godot;
 using System;
-
 public partial class dialog_trigger_area : Area2D
 {
     [Export(PropertyHint.File, "*json")]
     string dialogFile;
     public override void _Ready()
 	{
-
+        using var file = FileAccess.Open(dialogFile, FileAccess.ModeFlags.Read);
+        string text = file.GetAsText();
+        GD.Print(text);
 	}
 
 }
