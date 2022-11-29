0e6a8e660fd331dd0c7c5d60c75264656202cfae
vaporvee <yannik@vaporvee.com>
2022-11-29 19:29:03 +0100

    worked on the custom dialog system

--- src/scene-scripts/gui/dialog_bubble.cs
+++ src/scene-scripts/gui/dialog_bubble.cs
@@ -3,14 +3,13 @@ using System;
 
 public partial class dialog_bubble : CanvasLayer
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		Show();
-	}
+    public override void _Ready()
+    {
+        Show();
+        ImportString();
+    }
+    public void ImportString()
+    {
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    }
 }
---
+++ src/scene-scripts/gui/dialog_trigger_area.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+public partial class dialog_trigger_area : Area2D
+{
+    [Export(PropertyHint.File, "*json")]
+    string dialogFile;
+    public override void _Ready()
+	{
+
+	}
+
+}
