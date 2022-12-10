183c678d69320a16d2ccd6af251fb66fd466e2d1
vaporvee <yannik@vaporvee.com>
2022-12-10 14:54:00 +0100

    new player avatar

--- src/scene-scripts/player.cs
+++ src/scene-scripts/player.cs
@@ -25,16 +25,13 @@ public partial class player : CharacterBody2D
     public override void _Process(double delta)
     {
         //set ray_cast target position
-        if (Input.IsActionJustPressed("move_right")) GetNode<RayCast2D>("ray_cast_2d").TargetPosition = new Vector2(64, 0);
-        if (Input.IsActionJustPressed("move_left")) GetNode<RayCast2D>("ray_cast_2d").TargetPosition = new Vector2(-64, 0);
-        if (Input.IsActionJustPressed("move_down")) GetNode<RayCast2D>("ray_cast_2d").TargetPosition = new Vector2(0, 64);
-        if (Input.IsActionJustPressed("move_up")) GetNode<RayCast2D>("ray_cast_2d").TargetPosition = new Vector2(0, -64);
+        if (Input.IsActionJustPressed("move_right")) GetNode<RayCast2D>("ray_cast_2d").TargetPosition = new Vector2(128, 0);
+        if (Input.IsActionJustPressed("move_left")) GetNode<RayCast2D>("ray_cast_2d").TargetPosition = new Vector2(-128, 0);
+        if (Input.IsActionJustPressed("move_down")) GetNode<RayCast2D>("ray_cast_2d").TargetPosition = new Vector2(0, 128);
+        if (Input.IsActionJustPressed("move_up")) GetNode<RayCast2D>("ray_cast_2d").TargetPosition = new Vector2(0, -128);
 
         //call event in raycasted object
         if (Input.IsActionJustPressed("ui_accept") && GetNode<RayCast2D>("ray_cast_2d").IsColliding()) 
-        {
-            var raycastedObject = GetNode<RayCast2D>("ray_cast_2d").GetCollider();
-            raycastedObject.Call("OnInteraction", playerName);
-        }
+            GetNode<RayCast2D>("ray_cast_2d").GetCollider().Call("OnInteraction", playerName);
     }
 }
