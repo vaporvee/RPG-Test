e9fd520194eb10097e2028a5c1df9061bf7538f8
cmod31 <yannik@vaporvee.com>
2023-01-31 15:12:09 +0100

    more dialogue stuff

--- src/scene-scripts/gui/dialog_bubble.cs
+++ src/scene-scripts/gui/dialog_bubble.cs
@@ -3,7 +3,13 @@ using System;
 
 public partial class dialog_bubble : CanvasLayer
 {
+    
     public override void _Process(double delta)
     {
+    }
+    public static void SetDialog(string dialogFile)
+    {
+        var parsedDialog = Json.ParseString(FileAccess.Open(dialogFile, FileAccess.ModeFlags.Read).GetAsText());
+        json
     }
 }
