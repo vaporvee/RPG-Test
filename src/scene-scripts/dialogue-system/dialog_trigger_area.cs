2e2716f43acd6b9956b7910cf48d21f2d9c1755f
vaporvee <yannik@vaporvee.com>
2023-02-12 14:30:58 +0100

    changed to gdscript

--- src/scene-scripts/dialogue-system/dialog_trigger_area.cs
+++
@@ -1,9 +0,0 @@
-using Godot;
-using System;
-
-public partial class dialog_trigger_area : Area2D
-{
-    [Export(PropertyHint.File, "*json")] string file;
-    [Export] string title;
-    [Export] bool introducedVillager;
-}
---
+++ src/scene-scripts/dialogue-system/dialog_trigger_area.gd
@@ -0,0 +1,5 @@
+extends Area2D
+
+@export_file("*json") var file = ""
+@export var title = ""
+@export var introducedVillager = false
